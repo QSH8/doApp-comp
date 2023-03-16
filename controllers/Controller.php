9a3ad6fa113fe41d1ea6be3816bff3743197d7de
George <95532816+QSH8@users.noreply.github.com>
2023-03-16 04:29:58 +0300

    Update Controller.php

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -5,10 +5,10 @@ class Controller
     public $model;
     public $view;
     protected $pageData = array();
+    
     public function __construct()
     {
         $this->view = new View();
         $this->model = new Model();
-
     }
 }
