8d4b1fd4a14ede4ec0044a6ce77b0966baa98341
George <95532816+QSH8@users.noreply.github.com>
2023-03-16 04:32:28 +0300

    Update Router.php

--- routers/Router.php
+++ routers/Router.php
@@ -12,16 +12,13 @@ class Router
         if ($route[1]) {
             $controllerName = ucfirst($route[1] . "Controller");
             $modelName      = ucfirst($route[1] . "Model");
-
         }
-        
         include CONTROLLER_PATH . $controllerName . ".php";
         include MODEL_PATH      . $modelName      . ".php";
         
         if ($route[2]) {
             $action = $route[2];
         }
-
         $controller = new $controllerName();
         $controller->$action($_REQUEST);
     }
